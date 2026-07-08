37f3ab3e73a57edc453bfd26fc1cb61dd393653d
SirBlondieDK <sirblondiedk@gmail.com>
2026-07-08 22:45:00 +0200

    Remove legacy GUI compatibility modules and tests

--- tests/test_desktop_gui_import.py
+++
@@ -1,7 +0,0 @@
-def test_desktop_gui_import():
-    from src.hadocs.gui.desktop_app import HADocsDesktopApp, FirstRunWizard, SettingsDialog, AboutDialog
-
-    assert HADocsDesktopApp is not None
-    assert FirstRunWizard is not None
-    assert SettingsDialog is not None
-    assert AboutDialog is not None
